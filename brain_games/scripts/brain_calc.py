054b4b7f1054872f1e8ecac229e137e0943eb5e6
Onuchin <alex133@LAPTOP-Q7GG4FAM>
2023-04-22 16:37:27 +0300

    Correct some of linter notes

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -14,25 +14,25 @@ def main():
         operand_type = randint(1, 3)
         if operand_type == 1:
             print(f'Question: {num1} + {num2}')
-            correct_result = num1 + num2
+            cor_res = num1 + num2
         elif operand_type == 2:
             print(f'Question: {num1} - {num2}')
-            correct_result = num1 - num2
+            cor_res = num1 - num2
         else:
             print(f'Question: {num1} * {num2}')
-            correct_result = num1 * num2
+            corr_res = num1 * num2
         answer = prompt.string('Your answer: ')
         try:
             ans = int(answer)
         except ValueError:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_result}'.'")
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{cor_res}'.'")
             print(f"Let's try again, {name}!")
             break
-        if ans == correct_result:
+        if ans == corr_res:
             print('Correct!')
             i += 1
         else:
-            print(f"'{ans}' is wrong answer ;(. Correct answer was '{correct_result}'.'")
+            print(f"'{ans}' is wrong answer ;(. Correct answer was '{cor_res}'.'")
             print(f"Let's try again, {name}!")
             break
         if i >= 3:
